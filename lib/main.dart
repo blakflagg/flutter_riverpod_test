982b0850cf7dd2124ef0821f61052c96c1496625
= <=>
2024-06-13 17:02:58 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_test/screens/home_screen.dart';
+
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Riverpod Test",
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const HomeScreen(),
+      routes: {
+        HomeScreen.routeName: (ctx) => const HomeScreen(),
+      },
+      navigatorObservers: [routeObserver],
+    );
+  }
+}
